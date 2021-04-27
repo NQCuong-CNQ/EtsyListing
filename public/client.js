14c1b360ec6ec2e23c9a07fb30aa510c1006efd3
qcuongntrinh <nqcuong.cnq@gmail.com>
2021-04-27 10:41:36 +0700

    init

---
+++ public/client.js
@@ -0,0 +1,17 @@
+var socket = io.connect("http://localhost:80");
+
+socket.on("connect", function(data) {
+  socket.emit("join", { customId: "000CustomIdHere0000" });
+});
+
+socket.on("thread", function(data) {
+  $("#thread").append("<li>" + data + "</li>");
+});
+
+
+$("form").submit(function() {
+  var message = $("#message").val();
+  socket.emit("messages", message);
+  this.reset();
+  return false;
+});
