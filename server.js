5e77d905aee91c73c1556600798eb23063ecbf2b
qcuongntrinh <nqcuong.cnq@gmail.com>
2021-04-27 14:42:55 +0700

    test

--- server.js
+++ server.js
@@ -2,22 +2,18 @@
 var express = require("express");
 var app = express();
 var server = require("https").createServer(app);
-var io = require("socket.io")(server);
 
 
 require("greenlock-express")
   .init({
     packageRoot: __dirname,
-
-    // contact for security and critical bug notices
     configDir: "./greenlock.d",
     maintainerEmail: "jon@example.com",
-    // whether or not to run at cloudscale
     cluster: false
   })
-  // Serves on 80 and 443
-  // Get's SSL certificates magically!
-  .serve(app);
+  .serve(server);
+
+var io = require("socket.io")(server);
 
 
 app.get("/", function (req, res, next) {
