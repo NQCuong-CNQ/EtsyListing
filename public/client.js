031abc03d84e048675207fbe13cf71c8bb03582e
qcuongntrinh <nqcuong.cnq@gmail.com>
2021-04-27 13:50:39 +0700

    greenlock

--- public/client.js
+++ public/client.js
@@ -1,15 +1,15 @@
 var socket = io.connect("http://154.27.90.80:80");
 
-socket.on("connect", function(data) {
+socket.on("connect", function (data) {
   socket.emit("join", { customId: "000CustomIdHere0000" });
 });
 
-socket.on("thread", function(data) {
+socket.on("thread", function (data) {
   $("#thread").append("<li>" + data + "</li>");
 });
 
 
-$("form").submit(function() {
+$("form").submit(function () {
   var message = $("#message").val();
   socket.emit("messages", message);
   this.reset();
--- server.js
+++ server.js
@@ -1,25 +1,42 @@
+"use strict";
 var express = require("express");
 var app = express();
 var server = require("http").createServer(app);
 var io = require("socket.io")(server);
 
-app.get("/", function(req, res, next) {
+
+require("greenlock-express")
+  .init({
+    packageRoot: __dirname,
+
+    // contact for security and critical bug notices
+    configDir: "./greenlock.d",
+    maintainerEmail: "jon@example.com",
+    // whether or not to run at cloudscale
+    cluster: false
+  })
+  // Serves on 80 and 443
+  // Get's SSL certificates magically!
+  .serve(app);
+
+
+app.get("/", function (req, res, next) {
   res.sendFile(__dirname + "/public/index.html");
 });
 
 app.use(express.static("public"));
 
-io.on("connection", function(client) {
-    console.log("Client connected...");
+io.on("connection", function (client) {
+  console.log("Client connected...");
 
-    client.on("join", function(data) {
-      console.log(data.customId);
-    });
-
-    client.on("messages", function(data) {
-      console.log(data)
-    //   client.emit("thread", data);
-      client.broadcast.emit("thread", data);
-    });
+  client.on("join", function (data) {
+    console.log(data.customId);
   });
+
+  client.on("messages", function (data) {
+    console.log(data)
+    //   client.emit("thread", data);
+    client.broadcast.emit("thread", data);
+  });
+});
 server.listen(80, '154.27.90.80');
