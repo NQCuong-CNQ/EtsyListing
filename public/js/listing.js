1834740ca0b86b1729848de22a9bf23c86295c68
qcuongntrinh <nqcuong.cnq@gmail.com>
2021-04-27 17:54:33 +0700

    sdf

--- public/js/listing.js
+++ public/js/listing.js
@@ -1,1 +1,17 @@
-alert('ok')
+var socket = io.connect("https://giftsvk.com:80", {secure: true, rejectUnauthorized: false});
+
+socket.on("connect", function (data) {
+  socket.emit("join", { customId: "000CustomIdHere0000" });
+});
+
+socket.on("thread", function (data) {
+  $("#thread").append("<li>" + data + "</li>");
+});
+
+
+$("form").submit(function () {
+  var message = $("#message").val();
+  socket.emit("messages", message);
+  this.reset();
+  return false;
+});
