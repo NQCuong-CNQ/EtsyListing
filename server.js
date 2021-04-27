5abf0fdf33dad32d6bba4c98c3d321f259714e2c
qcuongntrinh <nqcuong.cnq@gmail.com>
2021-04-27 14:48:34 +0700

    tset

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 "use strict";
 var express = require("express");
 var app = express();
-var server = require("https").createServer(app);
+// var server = require("https").createServer(app);
 
-
+var app1 = require("./app.js");
 require("greenlock-express")
   .init({
     packageRoot: __dirname,
@@ -11,28 +11,28 @@ require("greenlock-express")
     maintainerEmail: "jon@example.com",
     cluster: false
   })
-  .serve(app);
+  .serve(app1);
 
-var io = require("socket.io")(server);
+// var io = require("socket.io")(server);
 
 
-app.get("/", function (req, res, next) {
-  res.sendFile(__dirname + "/public/index.html");
-});
+// app.get("/", function (req, res, next) {
+//   res.sendFile(__dirname + "/public/index.html");
+// });
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
-io.on("connection", function (client) {
-  console.log("Client connected...");
+// io.on("connection", function (client) {
+//   console.log("Client connected...");
 
-  client.on("join", function (data) {
-    console.log(data.customId);
-  });
+//   client.on("join", function (data) {
+//     console.log(data.customId);
+//   });
 
-  client.on("messages", function (data) {
-    console.log(data)
-    //   client.emit("thread", data);
-    client.broadcast.emit("thread", data);
-  });
-});
-server.listen(80, '154.27.90.80');
+//   client.on("messages", function (data) {
+//     console.log(data)
+//     //   client.emit("thread", data);
+//     client.broadcast.emit("thread", data);
+//   });
+// });
+// server.listen(80, '154.27.90.80');
