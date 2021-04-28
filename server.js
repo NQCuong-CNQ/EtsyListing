b2bdf415f9482714c29242117908114cbaee7cb2
qcuongntrinh <nqcuong.cnq@gmail.com>
2021-04-28 09:06:23 +0700

    sd

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 "use strict";
 var express = require("express");
 var app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "giftsvk.com"); 
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 require("greenlock-express")
   .init({
@@ -11,9 +17,11 @@ require("greenlock-express")
   })
   .serve(app);
 
+
 var server = require("https").createServer(app);
 
 var io = require("socket.io")(server);
+
 
 
 app.get("/", function (req, res, next) {
