3a8bbfd9b1e3519ee6e3bc2a1cbd4413b35dced4
qcuongntrinh <nqcuong.cnq@gmail.com>
2021-05-04 11:13:13 +0700

    test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 var express = require("express");
 var app = express();
 
-require("greenlock-express")
+require("../../")
   .init({
     packageRoot: __dirname,
     configDir: "./greenlock.d",
@@ -11,35 +11,63 @@ require("greenlock-express")
     cluster: false,
     approveDomains: ['giftsvk.com', 'www.giftsvk.com', 'localhost'],
   })
-  .serve(app);
+  .ready(httpsWorker);
 
-var server = require("https").createServer(app);
-var io = require("socket.io")(server);
 
-app.use(function cors(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-  next()
-})
+function httpsWorker(glx) {
+  var socketio = require("socket.io");
+  var io;
 
-app.get("/", function (req, res, next) {
-  res.sendFile(__dirname + "/public/index.html");
-});
+  // we need the raw https server
+  var server = glx.httpsServer();
 
-app.use(express.static("public"));
+  io = socketio(server);
 
-io.on("connection", function (client) {
-  console.log("Client connected...");
+  // Then you do your socket.io stuff
+  io.on("connection", function(socket) {
+      console.log("a user connected");
+      socket.emit("Welcome");
 
-  client.on("join", function (data) {
-    console.log(data.customId);
+      socket.on("chat message", function(msg) {
+          socket.broadcast.emit("chat message", msg);
+      });
   });
 
-  client.on("messages", function (data) {
-    console.log(data)
-    //   client.emit("thread", data);
-    client.broadcast.emit("thread", data);
+  // servers a node app that proxies requests to a localhost
+  glx.serveApp(function(req, res) {
+      res.setHeader("Content-Type", "text/html; charset=utf-8");
+      res.end("Hello, World!\n\n💚 🔒.js");
   });
-});
-server.listen(80);
+}
+
+
+// var server = require("https").createServer(app);
+// var io = require("socket.io")(server);
+
+// app.use(function cors(req, res, next) {
+//   res.setHeader('Access-Control-Allow-Origin', '*')
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+//   next()
+// })
+
+// app.get("/", function (req, res, next) {
+//   res.sendFile(__dirname + "/public/index.html");
+// });
+
+// app.use(express.static("public"));
+
+// io.on("connection", function (client) {
+//   console.log("Client connected...");
+
+//   client.on("join", function (data) {
+//     console.log(data.customId);
+//   });
+
+//   client.on("messages", function (data) {
+//     console.log(data)
+//     //   client.emit("thread", data);
+//     client.broadcast.emit("thread", data);
+//   });
+// });
+// server.listen(80);
