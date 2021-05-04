eb064ea5ae1b70587e2dbaf0df194cd9944ca38c
qcuongntrinh <nqcuong.cnq@gmail.com>
2021-05-04 11:28:14 +0700

    era

--- server.js
+++ server.js
@@ -28,21 +28,21 @@ function httpsWorker(glx) {
 
   app.use(express.static("public"))
 
-  io.on("connection", function(socket) {
-      console.log("a user connected")
-      socket.emit("Welcome")
+  // io.on("connection", function(socket) {
+  //     console.log("a user connected")
+  //     socket.emit("Welcome")
 
-      socket.on("chat message", function(msg) {
-          socket.broadcast.emit("chat message", msg)
-      });
-  });
+  //     socket.on("chat message", function(msg) {
+  //         socket.broadcast.emit("chat message", msg)
+  //     });
+  // });
 
   // servers a node app that proxies requests to a localhost
   // glx.serveApp(function(req, res) {
   //     res.setHeader("Content-Type", "text/html; charset=utf-8")
   //     res.end("Hello, World!\n\n💚 🔒.js")
   // })
-  // server.listen(80)
+  server.listen(80)
 }
 
 
