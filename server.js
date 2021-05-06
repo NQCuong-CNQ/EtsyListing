67a747e01b0c002bc654ae63ec3a95b4cf7ed584
qcuongntrinh <nqcuong.cnq@gmail.com>
2021-05-06 10:32:42 +0700

    ds

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ var app = express()
 var server = require("https").createServer(app)
 // var io = require("socket.io")(server)
 
-// app.use(function cors(req, res, next) {
-//   res.setHeader('Access-Control-Allow-Origin', '*')
-//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-//   next()
-// })
+app.use(function cors(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  next()
+})
 
 app.get("/", function (req, res, next) {
   res.sendFile(__dirname + "/public/index.html")
