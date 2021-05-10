e654911b2224f83d8ab440c10da05da34fb71911
qcuongntrinh <nqcuong.cnq@gmail.com>
2021-05-10 09:22:02 +0700

    fd

--- server.js
+++ server.js
@@ -167,7 +167,7 @@ app.get("/", function (req, res, next) {
 });
 
 app.use(express.static("public"))
-console.log('sdf')
+
 io.on("connection", async function (client) {
   let clientDB = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   var dbo = clientDB.db("trackingdb")
@@ -179,6 +179,7 @@ io.on("connection", async function (client) {
     console.log('1 client connected')
     // let dbData = await dbo.collection("shop").find({ total_sales: { $gte: 10 } }).toArray()
     let dbData = await dbo.collection("shop").find().toArray()
+    console.log('sdf')
     await client.emit("dataTransfer", dbData)
   })
 
