bc19e01174db77540a98fec2bf21892a027d8686
qcuongntrinh <nqcuong.cnq@gmail.com>
2021-05-20 15:12:55 +0700

    d

--- public/js/add_tracking_etsy.js
+++ public/js/add_tracking_etsy.js
@@ -1,7 +1,14 @@
 var socket = io.connect("https://giftsvk.com")
 
+sleep(5000)
 console.log("da ket noi")
 var id = '2053148040'
 
 let element = $(`[href="/your/orders/sold?page=1&order_id=${id}"]:eq(0)`).parent().parent().parent().parent().parent().parent().parent().parent().parent()[0]
 element.$('.wt-tooltip.wt-tooltip--bottom button')[1].click()
+
+async function sleep(ms) {
+    return new Promise(
+        resolve => setTimeout(resolve, ms)
+    );
+}
