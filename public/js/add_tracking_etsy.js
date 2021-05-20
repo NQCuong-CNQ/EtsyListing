38d5f2a4837a35f655fb6580c4a9a5e9b75a0603
qcuongntrinh <nqcuong.cnq@gmail.com>
2021-05-20 17:10:03 +0700

    sadf

--- public/js/add_tracking_etsy.js
+++ public/js/add_tracking_etsy.js
@@ -1,17 +1,27 @@
 var socket = io.connect("https://giftsvk.com")
 
+console.log("da ket noi !!!")
 socket.on("track-order-return", async function (data) {
-    console.log(data)
-    // await test()
+    for (var i = 0; i < data.length; i++){
+        console.log(data[i])
+        await addTracking(data[i].pro_ID, data[i].track_number)
+        await sleep(1000)
+        return
+    }
 })
 
-async function test() {
-
-    console.log("da ket noi")
-    var id = '2053148040'
-
-    let element = document.querySelector('[href="/your/orders/sold?page=1&order_id=2053148040"]').closest('.flag')
+async function addTracking(id, number) {
+    if(document.querySelector(`[href="/your/orders/sold?page=1&order_id=${id}"]`) == null){
+        return
+    }
+    let element = document.querySelector(`[href="/your/orders/sold?page=1&order_id=${id}"]`).closest('.flag')
     $(element).find(".wt-tooltip.wt-tooltip--bottom button")[1].click()
+
+    await sleep(4000)
+
+    $('[placeholder="Enter tracking number (recommended)"]:eq(0)').val(number)
+    
+
 }
 
 async function sleep(ms) {
