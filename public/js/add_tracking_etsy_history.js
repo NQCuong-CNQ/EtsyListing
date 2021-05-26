062ab4a58cdd33da9a7c9f6a511c7ec2e39db190
cuong <nqcuong9a5@gmail.com>
2021-05-26 11:23:55 +0700

    sadf

--- public/js/add_tracking_etsy_history.js
+++ public/js/add_tracking_etsy_history.js
@@ -16,8 +16,9 @@ function updateData(data) {
             <td>${i}</td>
             <td>${data[i].id}</td>
             <td>${data[i].name}</td>
+            <td>${data[i].carrier_name}</td>
             <td>${data[i].number_tracking}</td>
-            <td>${data[i].time_add_tracking}</td>
+            <td>${getEpochTime(data[i].time_add_tracking)}</td>
       </tr>`)
     }
 
@@ -26,3 +27,28 @@ function updateData(data) {
         order: [[0, "desc"]],
     })
 }
+function getEpochTime(input) {
+    var date = new Date(0)
+    date.setUTCSeconds(input)
+    time = String(date)
+    time = time.split(' ')
+    time = time[4] + ' ' + time[2] +'/'+convertMonthInString(time[1])
+    return time
+  }
+
+  function convertMonthInString(month) {
+    switch (month) {
+      case 'Jan': return '01'
+      case 'Feb': return '02'
+      case 'Mar': return '03'
+      case 'Apr': return '04'
+      case 'May': return '05'
+      case 'Jun': return '06'
+      case 'Jul': return '07'
+      case 'Aug': return '08'
+      case 'Sep': return '09'
+      case 'Oct': return '10'
+      case 'Nov': return '11'
+      case 'Dec': return '12'
+    }
+  }
--- test.js
+++ test.js
@@ -29,9 +29,29 @@
 
 
 // server.listen(8080)
-let listingData = 0
+getEpochTime()
+function getEpochTime() {
+    var date = new Date(0)
+    date.setUTCSeconds(1621997752)
+    time = String(date)
+    time = time.split(' ')
+    time = time[4] + ' ' + time[2] +'/'+convertMonthInString(time[1])
+    console.log(time)
+  }
 
-// let newData = []
-// newData[`${listingData}`] += 5+','
-
-console.log(listingData / 55)
+  function convertMonthInString(month) {
+    switch (month) {
+      case 'Jan': return '01'
+      case 'Feb': return '02'
+      case 'Mar': return '03'
+      case 'Apr': return '04'
+      case 'May': return '05'
+      case 'Jun': return '06'
+      case 'Jul': return '07'
+      case 'Aug': return '08'
+      case 'Sep': return '09'
+      case 'Oct': return '10'
+      case 'Nov': return '11'
+      case 'Dec': return '12'
+    }
+  }
