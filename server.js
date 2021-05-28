e18b54902cf77fa6e9cb67c7cb40853b689fada6
cuong <nqcuong9a5@gmail.com>
2021-05-28 11:32:23 +0700

    asd

--- server.js
+++ server.js
@@ -507,7 +507,9 @@ io.on("connection", async function (client) {
       trackObj['track_number'] = temp[i].split(',')[19].replace(/[^0-9a-zA-Z]/g, '')
       trackObj['order_status'] = temp[i].split(',')[3].replace(/[^0-9a-zA-Z]/g, '')
 
-      if (trackObj['track_number'].length > 10) {
+      console.log(trackObj['order_status'])
+
+      if (trackObj['track_number'] != '' && trackObj['order_status'] == 'Shipped') {
         trackData.push(trackObj)
       }
 
