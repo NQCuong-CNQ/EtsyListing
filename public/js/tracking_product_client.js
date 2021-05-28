c0fb45ecbee5b23133f5f8293f13eed182f10874
cuong <nqcuong9a5@gmail.com>
2021-05-28 16:28:27 +0700

    fd

--- public/js/tracking_product_client.js
+++ public/js/tracking_product_client.js
@@ -322,12 +322,12 @@ function handleDuplicates() {
     temp['percent_favor'] = listingData[lastPos].percent_favor
     temp['sales_day'] = 0
 
-    // if (arrPos.length > 1) {
-    //   let numDays = listingData[lastPos].date_update - Math.floor(listingData[lastPos].original_creation_tsz / 86400)
-    //   if (numDays > 1) {
-    //     console.log('arrPos.length'+arrPos.length)
-    //     let totalCount = 0
-    //     let diff = 0
+    if (arrPos.length > 1) {
+      let numDays = listingData[lastPos].date_update - Math.floor(listingData[lastPos].original_creation_tsz / 86400)
+      if (numDays > 1) {
+        console.log('arrPos.length'+arrPos.length)
+        let totalCount = 0
+        let diff = 0
     //     for (let j = arrPos.length - 1; j <= 1; j--) {
     //       diff = listingData[arrPos[j]].quantity - listingData[arrPos[j - 1]].quantity
     //       console.log('diff'+diff)
@@ -336,10 +336,10 @@ function handleDuplicates() {
     //       }
     //       totalCount += diff
     //     }
-    //     console.log('totalCount'+totalCount +'-'+numDays)
-    //     temp['sales_day'] = (totalCount / numDays).toFixed(2)
-    //   }
-    // }
+        console.log('totalCount'+totalCount +'-'+numDays)
+        temp['sales_day'] = (totalCount / numDays).toFixed(2)
+      }
+    }
     newData.push(temp)
   }
   listingData = newData
