49c345a2958a60f252bc018e563dfe7ad3da5f52
Quoc Cuong <nqcuong.cnq@gmail.com>
2021-05-29 15:36:48 +0700

    sdf

--- public/js/add_tracking_etsy_history.js
+++ public/js/add_tracking_etsy_history.js
@@ -265,7 +265,7 @@ function convertMonthInString(month) {
 }
 
 $('#fix-tracking-history-btn').on('click', function () {
-    this.classList.toggle("active")
+    this.classList.toggle("active-fix-tracking")
     var content = this.nextElementSibling
     if (content.style.display === "block") {
         content.style.display = "none"
