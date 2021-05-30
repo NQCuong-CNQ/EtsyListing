2162a89f633b4e6f0fc118e9c9d6119ff89777ad
Quoc Cuong <nqcuong.cnq@gmail.com>
2021-05-30 20:34:19 +0700

    sdf

--- server.js
+++ server.js
@@ -582,13 +582,13 @@ async function fixTrackingHistory() {
   let dbdata = await dbo.collection("tracking_etsy_history").find().toArray()
   for (let i = 0; i < dbdata.length; i++) {
     if (dbdata[i].customer_email == undefined) {
-      
-      // if (dbdata[i].customer_email.indexOf('Message history1') > 0) {
-      //   dbdata[i].customer_email = dbdata[i].customer_email.replace('Message history1', '')
-      //   await dbo.collection("tracking_etsy_history").updateOne({ id: dbdata[i].id }, { $set: { customer_email: dbdata[i].mail } }, { upsert: true })
-      // }
-    } else {console.log(dbdata[i].customer_email)}
-
+    } else {
+      console.log(dbdata[i].customer_email)
+      if (dbdata[i].customer_email.indexOf('Message history1') > 0) {
+        dbdata[i].customer_email = dbdata[i].customer_email.replace('Message history1', '')
+        await dbo.collection("tracking_etsy_history").updateOne({ id: dbdata[i].id }, { $set: { customer_email: dbdata[i].mail } }, { upsert: true })
+      }
+    }
   }
 }
 
