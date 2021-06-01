706cd779ca067e55590430129b57b2ec2b8095eb
Quoc Cuong <nqcuong.cnq@gmail.com>
2021-06-01 17:03:11 +0700

    df

--- public/js/tracking_product_client.js
+++ public/js/tracking_product_client.js
@@ -406,7 +406,7 @@ function showAnalytic(id) {
     toastr.clear()
     toastr.warning('Please wait until data is updated!')
   } else {
-    $('.popup-analytic-container').css('display', 'none')
+    $('.popup-analytic-container').css('display', 'block')
     $('.popup-analytic-background').css('display', 'block')
     let tempData = []
     for (let i = 0; i < dataOriginal.length; i++) {
