4aabaaf460af8d3382c63a67aca80bcdd9484bb1
Quoc Cuong <nqcuong.cnq@gmail.com>
2021-06-02 07:25:09 +0700

    sdf

--- public/js/add_tracking_etsy.js
+++ public/js/add_tracking_etsy.js
@@ -26,7 +26,12 @@ socket.on("get-email-customer-order", async function () {
 })
 
 socket.on("reload-etsy", function () {
+    if (shopName == 'lynnaustinLL') {
+        location.href = 'https://www.etsy.com/your/orders/sold?page=2'
+        return
+    }
     location.href = 'https://www.etsy.com/your/orders/sold'
+
 })
 
 socket.on("track-order-return", async function (dataReceive) {
@@ -51,22 +56,22 @@ socket.on("track-order-step4", async function (name) {
         let numCarrier = $('[for="Select shipping carrier..."]').val()
         let nameCarrier = $(`[for="Select shipping carrier..."] option[value="${numCarrier}"]`).text()
 
-        if(numCarrier === undefined || numCarrier == ''){
+        if (numCarrier === undefined || numCarrier == '') {
             numCarrier = $('[for="Select delivery carrier..."]').val()
             nameCarrier = $(`[for="Select delivery carrier..."] option[value="${numCarrier}"]`).text()
         }
 
-        if(numCarrier === undefined || numCarrier == ''){
+        if (numCarrier === undefined || numCarrier == '') {
             numCarrier = $('[for="Select delivery company..."]').val()
             nameCarrier = $(`[for="Select delivery company..."] option[value="${numCarrier}"]`).text()
         }
 
-        if (nameCarrier === 'Other'|| nameCarrier == '') {
+        if (nameCarrier === 'Other' || nameCarrier == '') {
             nameCarrier = $('input[placeholder="Shipping carrier"]').val()
-            if(nameCarrier === undefined || nameCarrier == ''){
+            if (nameCarrier === undefined || nameCarrier == '') {
                 nameCarrier = $('input[placeholder="Delivery carrier"]').val()
             }
-            if(nameCarrier === undefined || nameCarrier == ''){
+            if (nameCarrier === undefined || nameCarrier == '') {
                 nameCarrier = $('input[placeholder="Delivery company"]').val()
             }
         }
@@ -76,7 +81,7 @@ socket.on("track-order-step4", async function (name) {
             return
         }
 
-        if(actualInput.length <= 16){
+        if (actualInput.length <= 16) {
             return
         }
 
@@ -91,10 +96,10 @@ socket.on("track-order-step4", async function (name) {
         trackData['carrier_name'] = nameCarrier
         trackData['actual_input'] = actualInput
 
-        if(actualInput == trackData['number_tracking']){
+        if (actualInput == trackData['number_tracking']) {
             $('.position-absolute.position-bottom .flag-img button.btn-orange').trigger('click')
         } else {
-            return 
+            return
         }
 
         await socket.emit("track-order-step5", trackData)
@@ -107,6 +112,29 @@ socket.on("track-order-step4", async function (name) {
 async function addTrackingAction(id, number) {
     console.log(id + '/ ' + number)
     index++
+
+    if(shopName == 'lynnaustinLL'){
+        if (document.querySelector(`[href="/your/orders/sold?page=2&order_id=${id}"]`) == null || number.length < 10) {
+            await addTracking()
+            return
+        }
+        let element = document.querySelector(`[href="/your/orders/sold?page=2&order_id=${id}"]`).closest('.flag')
+        if (shopName == 'DennisGawlick') {
+            $(element).find(".wt-tooltip.wt-tooltip--bottom button")[0].click()
+        } else {
+            $(element).find(".wt-tooltip.wt-tooltip--bottom button")[1].click()
+        }
+    
+        await sleep(2000)
+    
+        trackData = new Object
+        trackData['name'] = shopName
+        trackData['id'] = id
+        trackData['number_tracking'] = number
+    
+        await socket.emit("track-order-step1", trackData)
+        return
+    }
 
     if (document.querySelector(`[href="/your/orders/sold?order_id=${id}"]`) == null || number.length < 10) {
         await addTracking()
