1097909ba6da02dce9805c91665f57cd2d2bf344
Quoc Cuong <nqcuong.cnq@gmail.com>
2021-06-09 17:33:15 +0700

    dfg

--- public/js/mockup.js
+++ public/js/mockup.js
@@ -1,3 +1,30 @@
+var canvas = document.getElementById('myCanvas')
+canvas.width = canvas.height = 1600
+canvas.style.width = canvas.style.height = "500px"
+var context = canvas.getContext('2d')
+
+var sources = {
+    image1: '/img/mockup/back.jpg',
+    image2: '/img/mockup/img.jpg',
+}
+
+loadImages(sources, function (images) {
+    context.drawImage(images.image1, 0, 0, 500, 500)
+    context.drawImage(images.image2, 350, 55, 93, 104)
+
+    var scale = 1600 / 500
+    context.setTransform(scale,0,0,scale,0,0)
+})
+
+function downloadCanvas(link, canvasId, filename) {
+    link.href = document.getElementById(canvasId).toDataURL()
+    link.download = filename
+}
+
+document.getElementById('download').addEventListener('click', function () {
+    downloadCanvas(this, 'myCanvas', 'test.png')
+}, false)
+
 function loadImages(sources, callback) {
     var images = {}
     var loadedImages = 0
@@ -16,27 +43,3 @@ function loadImages(sources, callback) {
         images[src].src = sources[src]
     }
 }
-var canvas = document.getElementById('myCanvas')
-var context = canvas.getContext('2d')
-
-var sources = {
-    image1: '/img/mockup/img.jpg',
-    image2: '/img/mockup/img2.jpg',
-    image3: '/img/mockup/img3.jpg'
-}
-
-loadImages(sources, function (images) {
-    context.drawImage(images.image1, 100, 30, 200, 137)
-    context.drawImage(images.image2, 350, 55, 93, 104)
-    context.drawImage(images.image2, 0, 55, 93, 104)
-})
-
-
-function downloadCanvas(link, canvasId, filename) {
-    link.href = document.getElementById(canvasId).toDataURL()
-    link.download = filename
-}
-
-document.getElementById('download').addEventListener('click', function () {
-    downloadCanvas(this, 'myCanvas', 'test.png')
-}, false)
