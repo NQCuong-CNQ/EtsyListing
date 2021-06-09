0a41f11615881e7059de9137aa7983c8a1612ba3
Quoc Cuong <nqcuong.cnq@gmail.com>
2021-06-09 17:02:33 +0700

    sdf

--- public/js/mockup.js
+++ public/js/mockup.js
@@ -1,15 +1,16 @@
 window.onload = function() {
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    var img = document.getElementById("scream");
-    ctx.drawImage(img, 10, 10);
+    var c = document.getElementById("myCanvas")
+    var ctx = c.getContext("2d")
+    var img = document.getElementById("scream")
+    ctx.globalCompositeOperation="destination-over"
+    ctx.drawImage(img, 10, 10)
   }
   
   function downloadCanvas(link, canvasId, filename) {
-      link.href = document.getElementById(canvasId).toDataURL();
-      link.download = filename;
+      link.href = document.getElementById(canvasId).toDataURL()
+      link.download = filename
   }
   
   document.getElementById('download').addEventListener('click', function() {
-      downloadCanvas(this, 'myCanvas', 'test.png');
-  }, false);
+      downloadCanvas(this, 'myCanvas', 'test.png')
+  }, false)
