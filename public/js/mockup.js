0ffba1ee422918aa0557296b7bed652ac2cb8b26
Quoc Cuong <nqcuong.cnq@gmail.com>
2021-06-09 17:17:31 +0700

    das

--- public/js/mockup.js
+++ public/js/mockup.js
@@ -1,10 +1,19 @@
 window.onload = function() {
+    var img1 = new Image()
+    img1.src = "/img/mockup/img.jpg"
+    var img2 = new Image()
+    img2.src = "/img/mockup/img2.jpg"
+    var img3 = new Image()
+    img3.src = "/img/mockup/img3.jpg"
+
     var c = document.getElementById("myCanvas")
     var ctx = c.getContext("2d")
     ctx.globalCompositeOperation="destination-over"
-    var img = document.getElementById("scream")
+    // var img = document.getElementById("scream")
     
-    ctx.drawImage(img, 100, 100, 100, 100)
+    ctx.drawImage(img1, 0, 100, 100, 100)
+    ctx.drawImage(img2, 100, 100, 100, 100)
+    ctx.drawImage(img3, 200, 100, 100, 100)
   }
   
   function downloadCanvas(link, canvasId, filename) {
