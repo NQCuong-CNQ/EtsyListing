669af1ad06bf43256e53a0e33998490580442071
cuong <nqcuong9a5@gmail.com>
2021-06-16 14:48:58 +0700

    dsf

--- server.js
+++ server.js
@@ -65,16 +65,17 @@ async function scheduleUpdate() {
 
 async function updateCate() {
   let category = {
-    'CategoryList': 'Canvas,Mug,Blanket,Shirt,Tumbler',
-    'CategoryLink': 'https://www.etsy.com/c/home-and-living/home-decor/wall-decor/wall-hangings/prints?explicit=1&ref=pagination&page=|https://www.etsy.com/c/home-and-living/kitchen-and-dining/drink-and-barware/drinkware/mugs?explicit=1&ref=pagination&page=|https://www.etsy.com/c/home-and-living/bedding/blankets-and-throws?ref=pagination&explicit=1&page=|https://www.etsy.com/c/clothing/mens-clothing/shirts-and-tees?ref=pagination&page=|https://www.etsy.com/c/home-and-living/kitchen-and-dining/drink-and-barware/drinkware/tumblers-and-water-glasses?explicit=1&ref=pagination&page=',
+    'CategoryList': 'Canvas,Canvas,Mug,Blanket,Shirt,Tumbler',
+    'CategoryLink': 'https://www.etsy.com/c/home-and-living/home-decor/wall-decor/wall-hangings/prints?explicit=1&ref=pagination&page=|https://www.etsy.com/c/handmade/home-and-living/home-decor/wall-decor/wall-hangings?explicit=1&facet=home-and-living%2Fhome-decor%2Fwall-decor%2Fwall-hangings&ship_to=US&attr_346=2341&item_type=handmade&ref=pagination&page=|https://www.etsy.com/c/home-and-living/kitchen-and-dining/drink-and-barware/drinkware/mugs?explicit=1&ref=pagination&page=|https://www.etsy.com/c/home-and-living/bedding/blankets-and-throws?ref=pagination&explicit=1&page=|https://www.etsy.com/c/clothing/mens-clothing/shirts-and-tees?ref=pagination&page=|https://www.etsy.com/c/home-and-living/kitchen-and-dining/drink-and-barware/drinkware/tumblers-and-water-glasses?explicit=1&ref=pagination&page=',
   }
+  await dbo.collection("category").remove()
   await dbo.collection("category").insertOne(category)
 }
 
 async function updateData() {
   isUpdate = true
   
-  // await updateCate()
+  await updateCate()
   await getListing()
   await getShopName()
   await updateShopInfo()
