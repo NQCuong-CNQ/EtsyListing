f4738a3e4a43a9da53a00021b2d38edc26860021
cuong <nqcuong9a5@gmail.com>
2021-06-25 17:29:06 +0700

    sdf

--- routers/main-router.js
+++ routers/main-router.js
@@ -2,27 +2,27 @@ var express = require("express")
 var router = express.Router()
 
 router.get("/", function (req, res) {
-    res.sendFile("../public/index.html")
+    res.sendFile(__dirname + "/public/index.html")
 })
 
 router.get("/tracking-shop", function (req, res) {
-    res.sendFile("../public/tracking_shop.html")
+    res.sendFile(__dirname + "/public/tracking_shop.html")
 })
 
 router.get("/tracking-product", function (req, res) {
-    res.sendFile("../public/tracking_product.html")
+    res.sendFile(__dirname + "/public/tracking_product.html")
 })
 
 router.get("/tools", function (req, res) {
-    res.sendFile("../public/tools.html")
+    res.sendFile(__dirname + "/public/tools.html")
 })
 
 router.get("/listing", function (req, res) {
-    res.sendFile("../public/etsy_listing.html")
+    res.sendFile(__dirname + "/public/etsy_listing.html")
 })
 
 router.get("/add_tracking_history", function (req, res) {
-    res.sendFile("../public/add_tracking_etsy_history.html")
+    res.sendFile(__dirname + "/public/add_tracking_etsy_history.html")
 })
 
 router.get("/undefined", function (req, res) {
@@ -30,7 +30,7 @@ router.get("/undefined", function (req, res) {
 })
 
 router.get("/mockup", function (req, res) {
-    res.sendFile("../public/mockup.html")
+    res.sendFile(__dirname + "/public/mockup.html")
 })
 
 module.exports = router
