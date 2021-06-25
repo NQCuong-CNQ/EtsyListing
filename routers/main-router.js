f4a82b2094c1e70a8822c18e6851f972bd0a4b46
cuong <nqcuong9a5@gmail.com>
2021-06-25 17:36:49 +0700

    e

--- routers/main-router.js
+++ routers/main-router.js
@@ -2,28 +2,28 @@ var express = require("express")
 var router = express.Router()
 
 router.get("/", function (req, res) {
-    console.log(__dirname + "/../public/index.html")
-    res.sendFile(__dirname + "/../public/index.html")
+    console.log(__dirname + "/public/index.html")
+    res.sendFile(__dirname + "/public/index.html")
 })
 
 router.get("/tracking-shop", function (req, res) {
-    res.sendFile(__dirname + "/../public/tracking_shop.html")
+    res.sendFile(__dirname + "/public/tracking_shop.html")
 })
 
 router.get("/tracking-product", function (req, res) {
-    res.sendFile(__dirname + "/../public/tracking_product.html")
+    res.sendFile(__dirname + "/public/tracking_product.html")
 })
 
 router.get("/tools", function (req, res) {
-    res.sendFile(__dirname + "/../public/tools.html")
+    res.sendFile(__dirname + "/public/tools.html")
 })
 
 router.get("/listing", function (req, res) {
-    res.sendFile(__dirname + "/../public/etsy_listing.html")
+    res.sendFile(__dirname + "/public/etsy_listing.html")
 })
 
 router.get("/add_tracking_history", function (req, res) {
-    res.sendFile(__dirname + "/../public/add_tracking_etsy_history.html")
+    res.sendFile(__dirname + "/public/add_tracking_etsy_history.html")
 })
 
 router.get("/undefined", function (req, res) {
@@ -31,7 +31,7 @@ router.get("/undefined", function (req, res) {
 })
 
 router.get("/mockup", function (req, res) {
-    res.sendFile(__dirname + "/../public/mockup.html")
+    res.sendFile(__dirname + "/public/mockup.html")
 })
 
 module.exports = router
