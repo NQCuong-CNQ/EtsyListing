002cb8d5bc2b646d1340d06e4c6d4a3258a9a5f3
Quoc Cuong <nqcuong.cnq@gmail.com>
2021-06-28 13:38:05 +0700

    sdf

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -31,5 +31,5 @@ module.exports.postLogin = async function(req, res){
         return
     }
     console.log('ok')
-    res.redirect('/dashboard')
+    res.sendFile(dirname + "public/views/index.html")
 }
