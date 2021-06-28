2420ddec2cc2a4b17b24aecfc2c9defa96088ce3
Quoc Cuong <nqcuong.cnq@gmail.com>
2021-06-28 11:14:01 +0700

    asdf

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,4 +1,4 @@
-var dirname = __dirname.slice(0, -7)
+var dirname = __dirname.slice(0, -11)
 
 module.exports.login = function(req, res){
     res.sendFile(dirname + "public/views/add_tracking_etsy_history.html")
