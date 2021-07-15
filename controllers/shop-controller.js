9f7328b7ebad84138e27f441059cdfa38354e9d8
Quoc Cuong <nqcuong.cnq@gmail.com>
2021-07-15 20:15:23 +0700

    sdf

--- controllers/shop-controller.js
+++ controllers/shop-controller.js
@@ -18,16 +18,15 @@ module.exports.getAll = async function (req, res) {
         let search = parseInt(req.query.search)
         let sort_by = parseInt(req.query.sort_by)
 
-        let data
+        let data = ''
 
         let shopCategory = await dbo.collection("shopCategory").find().toArray()
-        let dbData = await dbo.collection("shop").find().skip(offset).limit(2).toArray()
+        data = await dbo.collection("shop").find().skip(offset).limit(limit).toArray()
         let lastUpdated = await dbo.collection("log").find().sort({ $natural: -1 }).limit(1)
 
-        console.log(dbData) 
         // data = searchOrFilterData(shopCategory, dbData, type, category, month, sales)
         res.send({
-            shopData: dbData,
+            shopData: data,
             lastUpdated: lastUpdated,
         })
     } catch (err) {
