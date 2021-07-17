1e01497b2633ddd272b4052fa9b7e31373040899
Quoc Cuong <nqcuong.cnq@gmail.com>
2021-07-17 22:22:17 +0700

    restore

--- middleware/auth-middleware.js
+++ middleware/auth-middleware.js
@@ -2,7 +2,6 @@ const MongoClient = require('mongodb').MongoClient
 const url = "mongodb://localhost:27017/trackingdb"
 var clientDB
 var dbo
-// const jwt = require('jsonwebtoken')
 
 module.exports.requireAuth = async function(req, res, next){
     clientDB = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -21,15 +20,3 @@ module.exports.requireAuth = async function(req, res, next){
 
     next()
 }
-
-// module.exports.verify = function (req, res) {
-//     try {
-//         let token = req.cookies.token
-//         let result = jwt.verify(token, 'pass')
-//         if (result) {
-//             res.redirect('/')
-//         }
-//     } catch (err) {
-//         res.redirect('/login')
-//     }
-// }
--- public/js/login.js
+++ public/js/login.js
@@ -4,29 +4,6 @@ $('.js-tilt').tilt({
     scale: 1.1
 })
 
-$('.login-form-btn').on('click', () => {
-    try {
-        $.ajax({
-            url: '/login',
-            type: "post",
-            contentType: "application/json",
-            dataType: "json",
-            data: {
-                user_name: $('#user_name').val().trim(),
-                password: $('#password').val().trim(),
-            },
-            success: function (data) {
-                document.cookie = `token=${data.token}`
-            },
-            error: (jqXHR, textStatus, errorThrown) => {
-                console.log(jqXHR, textStatus, errorThrown)
-            }
-        })
-    } catch (err) {
-        console.log(err)
-    }
-})
-
 $('.validate-form .input').each(function () {
     $(this).on('focus', function () {
         hideValidate(this);
