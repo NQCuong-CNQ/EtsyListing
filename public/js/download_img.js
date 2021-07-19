9a92417033e781325f9e6e203319ecaf6390f940
Quoc Cuong <nqcuong.cnq@gmail.com>
2021-07-19 12:08:40 +0700

    à

--- public/js/download_img.js
+++ public/js/download_img.js
@@ -1,7 +1,7 @@
 var imgs = document.images
 var arrImgs = []
 for (let i = 0; i < imgs.length; i++) {
-    if (imgs[i].offsetWidth > 250 && imgs[i].offsetHeight > 250) {
+    if (imgs[i].naturalWidth > 250 && imgs[i].naturalHeight > 250) {
         console.log(imgs[i].currentSrc)
         arrImgs.push(imgs[i].currentSrc)
     }
