c196941b759ea6bcec836d120cb366fd966764b3
Quoc Cuong <nqcuong.cnq@gmail.com>
2021-07-21 07:41:11 +0700

    sdf

--- controllers/get-website-controller.js
+++ controllers/get-website-controller.js
@@ -2,12 +2,12 @@ const axios = require("axios")
 const cheerio = require('cheerio')
 
 module.exports.getWebsite = async function (req, res) {
-    console.log(req)
     let shopName = req.query.shop
     console.log('check ' + shopName)
     let siteUrl = `https://www.etsy.com/search/shops?search_type=shop&search_query=${shopName}`
     let result = await getShopAvailable(siteUrl)
     console.log(result)
+    result = result.replace('ressult', '').trim()
     res.send(result)
 }
 
@@ -19,7 +19,7 @@ async function getShopAvailable(siteUrl) {
 
     let shopName = $('#content').text()
     shopName = shopName.split('for shop names containing')
-    shopName = shopName[0].slice(15).trim()
+    shopName = shopName[0].slice(15)
 
     return shopName
 }
