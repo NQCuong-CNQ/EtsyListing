dd326f2f54a8605fa30a30ab07ea0a89fb3950fd
Quoc Cuong <nqcuong.cnq@gmail.com>
2021-08-12 10:45:41 +0700

    qewr

--- public/js/thao.js
+++ public/js/thao.js
@@ -4,7 +4,7 @@ $('#btn-ok').on('click', function(){
 })
 
 $('#you-wish-btn').on('mousedown', function(){
-    let x = Math.random(300)
-    let y = Math.random(200)
+    let x = Math.random(300) * 100
+    let y = Math.random(200) * 100
     $('#you-wish-btn').css('transform', `translate(${x}px,${y}px)`)
-})
+})  
