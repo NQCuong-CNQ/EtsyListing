03071d7ca09d53e7f1cb7cfd38e79c09ab4acfa8
Quoc Cuong <nqcuong.cnq@gmail.com>
2021-08-12 11:36:39 +0700

    sadf

--- public/js/thao_log.js
+++ public/js/thao_log.js
@@ -6,9 +6,19 @@ var socket = io.connect("https://giftsvk.com", {
 
 socket.emit("get-log-thao")
 
+getEpochTime = input => {
+    var date = new Date(0)
+    date.setUTCSeconds(input)
+    time = String(date)
+    console.log(time)
+    time = time.split(' ')
+    time = `${time[2]}-${convertMonthInString(time[1])}-${time[3]}`
+    return time
+}
+
 socket.on("return-get-log-thao", data => {
     console.log(data)
     for (let i = 0; i < data.length; i++) {
-        $('#logs').append(`<h2>${data[i].text}   -   ${data[i].time}</h2>`)
+        $('#logs').append(`<h2>${data[i].text} - ${getEpochTime(data[i].time)}</h2>`)
     }
 })
