82bfb516c7b2ff1517defbd352fae9ffa9b5740f
Quoc Cuong <nqcuong.cnq@gmail.com>
2021-08-12 09:52:44 +0700

    ádf

--- public/js/thao.js
+++ public/js/thao.js
@@ -2,3 +2,7 @@ $('#btn-ok').on('click', function(){
     $('.first-page').css('display', 'none')
     $('.second-page').css('display', 'block')
 })
+
+$('#you-wish-btn').on('mousedown', function(){
+    $('#you-wish-btn').css('transform', 'translate(100,100)')
+})
